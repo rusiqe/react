643e55bd594da236097b9a64dd2356f0d75c6fda
Taurai. M <rusiqe@gmail.com>
2023-06-27 18:00:50 +0000

    setup watch

---
+++ stopwatch-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BsStopwatch } from 'react-icons/bs'
+function App() {
+    const [stopwatchData, setStopwatchData] = useState([
+        { id: 0, name: "Timer 1", time: 0, isRunning: false, lap: [], time_started: 0, pause: [] },
+       ])
+       const [selectedRow, setSelectedRow] = useState(0)
+ return (
+  <div className="App">
+   <div className='container'>
+    <div className='left'>
+      
+    </div>
+    <div className='right'>
+      
+    </div>
+   </div>
+  </div>
+ );
+}
+ 
+export default App;
